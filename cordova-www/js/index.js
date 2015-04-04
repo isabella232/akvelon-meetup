b23122fa334e3a8e5c4c516fab76446d0bc2f11f
apla <dot.and.thing@gmail.com>
2015-04-04 12:26:33 +0300

    differentiate between firmwares

--- cordova-www/js/index.js
+++ cordova-www/js/index.js
@@ -26,12 +26,13 @@ var app = {
 	initialize: function() {
 		this.bindEvents();
 		detailPage.hidden = true;
+		this.currentDevice = null;
 	},
 	bindEvents: function() {
-		document.addEventListener('deviceready', this.onDeviceReady, false);
-		refreshButton.addEventListener('touchstart', this.refreshDeviceList, false);
-		closeButton.addEventListener('touchstart', this.disconnect, false);
-		deviceList.addEventListener('touchstart', this.connect, false); // assume not scrolling
+		document.addEventListener('deviceready', this.onDeviceReady.bind (this), false);
+		refreshButton.addEventListener('touchstart', this.refreshDeviceList.bind (this), false);
+		closeButton.addEventListener('touchstart', this.disconnect.bind (this), false);
+		deviceList.addEventListener('touchstart', this.connect.bind (this), false); // assume not scrolling
 	},
 	onDeviceReady: function() {
 		app.refreshDeviceList();
@@ -48,15 +49,55 @@ var app = {
 			device.uuid;
 
 		listItem.setAttribute('uuid', device.uuid);
+		listItem.setAttribute('data-name', device.advertising);
 		listItem.innerHTML = html;
 		deviceList.appendChild(listItem);
 	},
 	connect: function(e) {
 		var uuid = e.target.getAttribute('uuid'),
+			deviceName = e.target.getAttribute('data-name'),
 			onConnect = function() {
-				rfduino.onData(app.onData, app.onError);
+				console.log (deviceName);
+				if (deviceName === 'temp') {
+					rfduino.onData(app.onData, app.onError);
+				}
 				app.showDetailPage();
-			};
+				this.currentDeviceName = deviceName;
+				if (deviceName === 'ledbtn') {
+					app.serverIntervalHandle = setInterval (function () {
+						function ok (data) {
+							var bodyData = JSON.parse (data.body);
+							if (data.code === 200) {
+								document.getElementById ("tempLast").innerHTML = bodyData.data[0].temp;
+							}
+
+							console.log (data.code, bodyData);
+
+							if (parseFloat (bodyData.data[0].temp) > 30) {
+								var data = new Uint8Array(1);
+								data[0] = 0x1;
+
+								rfduino.write(data.buffer); // ignoring callbacks
+							} else {
+								var data = new Uint8Array(1);
+								data[0] = 0x0;
+
+								rfduino.write(data.buffer); // ignoring callbacks
+							}
+						}
+
+						function fail (err) {
+							console.log (err);
+						}
+
+						plugins.HTTPClient.request (ok, fail, {
+							uri: 'http://apla.me:50100/last.json'
+						});
+
+					}, 2000);
+				}
+
+			}.bind (this);
 
 		rfduino.connect(uuid, onConnect, app.onError);
 	},
@@ -74,7 +115,7 @@ var app = {
 				document.getElementById ("tempLast").innerHTML = bodyData.data[0].temp;
 			}
 
-			console.log (data);
+			console.log (data.code, bodyData);
 		}
 
 		function fail (err) {
@@ -82,11 +123,15 @@ var app = {
 		}
 
 		plugins.HTTPClient.request (ok, fail, {
-			uri: 'http://apla.me:50100/last.json'
+			uri: 'http://apla.me:50100/add.json?temp='+celsius.toFixed
 		});
 	},
 	disconnect: function() {
 		rfduino.disconnect(app.showMainPage, app.onError);
+
+		if (app.serverIntervalHandle) {
+			clearInterval (app.serverIntervalHandle);
+		}
 	},
 	showMainPage: function() {
 		mainPage.hidden = false;
